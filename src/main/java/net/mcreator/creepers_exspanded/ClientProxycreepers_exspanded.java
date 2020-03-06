d97c9a8c20424208154143dc817b9f5d14a43a6a
Absolut <sirtolken@gmail.com>
2020-03-06 15:43:47 -0500

    First Commit

---
+++ src/main/java/net/mcreator/creepers_exspanded/ClientProxycreepers_exspanded.java
@@ -0,0 +1,26 @@
+package net.mcreator.creepers_exspanded;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.obj.OBJLoader;
+
+public class ClientProxycreepers_exspanded implements IProxycreepers_exspanded {
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		OBJLoader.INSTANCE.addDomain("creepers_exspanded");
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
